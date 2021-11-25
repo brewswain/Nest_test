89ae92e563e65cb21c13ffff4cb1cbc9ffc986f7
Brandon Lee <brandonlee2543@hotmail.com>
2021-11-24 21:55:16 -0400

    Added demo SQL data

---
+++ events.sql
@@ -0,0 +1,8 @@
+SET NAMES 'UTF8';
+
+INSERT INTO event(id, description, event_date, address, name) VALUES
+(1,	'Let''s meet together.',	'2021-02-15 21:00:00',	'Office St 120',	'Team Meetup'),
+(2,	'Let''s learn something.',	'2021-02-17 21:00:00',	'Workshop St 80',	'Workshop'),
+(3,	'Let''s meet with big bosses',	'2021-02-17 21:00:00',	'Boss St 100',	'Strategy Meeting'),
+(4,	'Let''s try to sell stuff',	'2021-02-11 21:00:00',	'Money St 34',	'Sales Pitch'),
+(5,	'People meet to talk about business ideas',	'2021-02-12 21:00:00',	'Invention St 123',	'Founders Meeting');
