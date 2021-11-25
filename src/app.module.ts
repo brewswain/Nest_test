a8d352908bb65189fef6bcc567b5c50c7c810a6f
Brandon Lee <brandonlee2543@hotmail.com>
2021-11-24 20:27:44 -0400

    Connected app to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController, TestController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
@@ -6,7 +7,18 @@ import { EventsController } from './events.controller';
 // Module is a class using the @Module Decorator.
 // As we can see, here's where we import our controllers, providers, etc
 @Module({
-  imports: [],
+  // Let's use the imports array here to use our TypeOrmModule. .forRoot()
+  // is used to provide configuration that the module is going to use
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'example',
+      database: 'nest-events',
+    }),
+  ],
   controllers: [AppController, TestController, EventsController],
   providers: [AppService],
 })
