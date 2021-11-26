1a6939d7f44c0b51adab7e831e54cd45de4dd763
Brandon Lee <brandonlee2543@hotmail.com>
2021-11-25 22:51:33 -0400

    Update main.ts

--- src/1_basic_structure/main.ts
+++ src/1_basic_structure/main.ts
@@ -1,12 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Our entry point. This is where we spin up our server, etc etc.
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(new ValidationPipe());
-
   await app.listen(3000);
 }
 bootstrap();
